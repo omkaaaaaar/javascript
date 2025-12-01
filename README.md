ed32fe2d0fe8bc4a416adf7ce3d112dea99ff0b6
omkaaaaaar <omkarpatkarr@gmail.com>
2025-12-01 23:58:17 +0530

    d

--- README.md
+++ README.md
@@ -31,3 +31,90 @@ to log on the console(get o/p in console) we used the second line where we calle
      let firstName = 'abc';
      letlastName = 'xyz';
      ```
+
+---
+
+**Constants** in js
+
+1.  Use them when there is no need to re-assign variables
+2.  The value of the variable here doesn't changes
+
+---
+
+**Types**
+
+- There are two different types of types,
+  1. Primitives/ Value Types
+  2. Reference Types
+
+1. Primitives/ Value Types
+   categories
+
+- String
+- Number
+- Boolean
+- undefined : It is a type and also a Value
+- null : we use this type when we want to explicitly declare the value of the variable, ex if user wanted to select a color and doesnt selects then; ->
+  let selectedColor = null;
+
+2. Reference Types
+
+- Objects
+- Array
+- Function
+
+**Dynamic Typing**
+JS is a dynamic language, a type of a variable can bechanged later on/ in the future
+ex-
+
+```
+>typeof name
+< "string"
+> name = 1;
+< 1
+> typeof name
+< "number"            //the type got changed when we updated the value inside var
+```
+
+- we dont have float in JS, we only have number here, where even 30.5 is considered as a number instead of a float
+
+---
+
+**object** ln. 37
+
+Object in js is like an obj in real life, ex a person -> the person has name, age, address, etc These are the properties of a person
+We have the same concept in Js
+
+- insted of having multiple variables for a person
+- we can create a single object to represent a person
+- and instead of refrencing these 2 variables we can refrence the person object
+  ex-
+
+```
+let person = {      //{} are called object literal
+    name:'Omkar',
+    age:21
+}
+```
+
+There are two ways of accessing the contents property
+
+1. Dot Notation:-
+
+```
+//dot notation
+person.name = 'Omkar Patkar'; //we can change the value of name property using dot notation
+```
+
+2. Bracket Notation:-
+   Instead of Dot we here use a string, we pass a str that determines the name of the target property
+
+**The Dot Notation should be your preffered choice because it is concise**
+
+---
+
+**Arrays**
+Sometimes we deal with a list of objects, ex-list of products in a shopping cart, list of colors that user has selected. In this situation we use arrays to store the list
+
+- we can store different data types in an array, ex an array with str can store int too!
+- the array is automatically created as an object in js, and inherits properties like every,fill, filter,findIndex,forEach,length etc
--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 console.log("Hello World!");
 
+
+
 // Variables
 // let name;
 // console.log(name); // opundefined
 
-let name ='John';
-console.log(name); // opJohn
+// let name ='John';
+// console.log(name); // opJohn
+
 
 
 //Constants
-let interestRate = 0.3;
-interestRate = 1; //the value of interestRate changes from 0.3 to 1
-console.log(interestRate); // op=1
+
+// let interestRate = 0.3;
+// interestRate = 1; //the value of interestRate changes from 0.3 to 1
+// console.log(interestRate); // op=1
 
 //ts will create bugs in our application
 // soo we will use const
@@ -19,3 +23,48 @@ console.log(interestRate); // op=1
 const interestRateConst = 0.3;
 // interestRateConst = 1; // if we use this ln then we will get error because we cannot change the value of constant
 console.log(interestRateConst); // op=0.3
+
+
+// //Primitives
+// let name ='Mosh'; // string literal
+// let age = 30; // number literal
+// let isApproved = false; // boolean literal <- Trues and False are resereved words in js
+// let firstName = undefined; // undefined literal
+// let lastName = null; // null literal
+
+
+
+// //objects
+// let name = 'Omkar';
+// let age = 21;
+//they are part of the representatin of a person object, so insted of having multiple variables for a person
+//we can create a single object to represent a person
+//and instead of refrencing these 2 variables we can refrence the person object
+
+let person = {      //{} are called object literal
+    name:'Omkar',
+    age:21
+}
+
+//dot notation
+person.name = 'Omkar Patkar'; //we can change the value of name property using dot notation
+
+// console.log(person); //op= {name: 'Omkar Patkar', age: 21}
+console.log(person.name); //op= Omkar Patkar
+
+//bracket notation
+let selection = 'name'; // we can use bracket notation when we dont know the property name at the time of writing the code
+person[selection] = 'Patkar Omkar'; //we can change the value of name property using bracket notation
+console.log(person.name); //op= Patkar Omkar
+
+
+
+//Arrays
+let selectedColors = ['red', 'blue']; // [] are called array literal, they indicate an empty array
+//console.log(selectedColors); //op= ['red', 'blue']
+selectedColors[2] = 'grey'; // we can add new elements to an array using index
+selectedColors[3] = 5; // we can add different types of elements to an array
+console.log(selectedColors); //op= ['red', 'blue', 'grey', 5]
+console.log(selectedColors[0]); //op= red
+console.log(selectedColors[1]); //op= blue
+console.log(selectedColors.length); //op= 2
